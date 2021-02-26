c10d78bff9e9d344192bdd3edfc6e4dbd75eb70b
Jesond <js.pineda.sa@gmail.com>
2021-02-26 02:07:31 -0600

    The organization class was added

---
+++ organizacion.js
@@ -0,0 +1,13 @@
+// organización.js
+
+/** Clase que representa una organización */
+
+class Organizacion {
+  constructor(name, address, phone, representative, email) {
+    this.name = name; // nombre de la organización
+    this.address = address; // dirección
+    this.phone = phone; // número de telefono
+    this.representative = representative; // representante de la organización
+    this.email = email; // correo electrónico
+  }
+}
