786fa1d8f0738ad33280f1d72129d1dd30db53a2
raquel-vazquez <lvazquez@novasolutionsystems.com>
2021-02-26 10:26:10 -0600

    feat: crate class Donacion

---
+++ donacion.js
@@ -0,0 +1,10 @@
+/** Clase que representa una donacion */
+class Donacion {
+    constructor(id_donacion, id_donador, id_organizacion, cantidad, status) {
+      this.id_donacion = id_donacion; // número de indentificación de la donacion
+      this.id_donador = id_donador; // número de indentificación del donador
+      this.id_organizacion = id_organizacion; // número de indentificación de la organización
+      this.cantidad = cantidad; // cantidad de la donación
+      this.status = status; // estado de la donacion
+    }
+  }
