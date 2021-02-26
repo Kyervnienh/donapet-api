7de496429c126c6c3e80adf087d11fa7f39b0d4b
Jesond <js.pineda.sa@gmail.com>
2021-02-26 11:27:22 -0600

    fixed

--- organizacion.js
+++ organizacion.js
@@ -1,13 +1,19 @@
-// organización.js
-
 /** Clase que representa una organización */
 
 class Organizacion {
-  constructor(name, address, phone, representative, email) {
-    this.name = name; // nombre de la organización
-    this.address = address; // dirección
-    this.phone = phone; // número de telefono
-    this.representative = representative; // representante de la organización
-    this.email = email; // correo electrónico
+  constructor(
+    id_organizacion,
+    nombre,
+    direccion,
+    telefono,
+    representante,
+    correo
+  ) {
+    this.id_organizacion = id_organizacion; // número de indentificación de la organización
+    this.nombre = nombre; // nombre de la organización
+    this.direccion = direccion; // dirección
+    this.telefono = telefono; // número de telefono
+    this.representante = representante; // representante de la organización
+    this.correo = correo; // correo electrónico
   }
 }
