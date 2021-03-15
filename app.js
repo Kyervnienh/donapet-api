f8ff5d15aabbbdadb94d2defb9d0836f4729944c
raquel-vazquez <lvazquez@novasolutionsystems.com>
2021-03-14 19:51:19 -0600

    fix: add images

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Agregamos el código de nuestro router (routes/index.js)
+require('./config/passport');
+
 app.use('/v1', require('./routes'));
 
 // Manejando los errores 404
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
