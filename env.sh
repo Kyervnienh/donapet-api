73c4c160b9efa986fcddd3ec4669098cc1f229e2
Kevin L <72113431+Kyervnienh@users.noreply.github.com>
2021-03-15 09:57:09 -0600

    Delete env.sh

--- env.sh
+++
@@ -1,7 +0,0 @@
-export NODE_ENV='development'
-export PORT=3000
-export SECRET='secret'
-export MYSQL_USER='ugmtfah9vxn9hbsq'
-export MYSQL_PASSWORD='8Wuhw88GLlEDIRA6R3nW'
-export MYSQL_DB='bvjvt3algliwhdhkghed'
-export MYSQL_HOST='bvjvt3algliwhdhkghed-mysql.services.clever-cloud.com'
