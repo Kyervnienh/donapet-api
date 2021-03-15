b211a5ef812d1328e17c1023ad2cbfbe6c24e5a8
raquel-vazquez <lvazquez@novasolutionsystems.com>
2021-03-14 23:12:40 -0600

    feat: add Swagger documentation for Donaciones router

--- app.js
+++ app.js
@@ -15,13 +15,47 @@ require('./config/passport');
 app.use('/v1', require('./routes'));
 
 // Manejando los errores 404
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
 // Iniciando el servidor...
 var server = app.listen(process.env.PORT || 3000, function () {
   console.log('Escuchando en el puerto ' + server.address().port);
 });
+
+
+/**
+ * Importacion de Swagger
+ */
+
+ const swaggerUI = require("swagger-ui-express");
+ const swaggerJsDoc = require("swagger-jsdoc");
+ /**
+ * Informacion acerca de la API
+ */
+
+  const swaggerOptions = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "DonaPet API",
+        version: "1.0.0",
+        description: "PI para realizar donaciones a instituciones a cuidado de los animales",
+      },
+      servers: [
+        {
+          url: "http://localhost:3000",
+        },
+      ],
+    },
+    apis: ["./routes/*.js"],
+  };
+  
+ const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+ app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+
+
--- routes/donaciones.js
+++ routes/donaciones.js
@@ -9,10 +9,170 @@ const {
 } = require('../controllers/donaciones')
 const auth = require('./auth');
 
-router.post('/', auth.requerido, crearDonacion);
-router.put('/:id', auth.requerido, modificarDonacion);
-router.get('/', auth.requerido, consultarDonaciones);
-router.get('/:id', auth.requerido, consultarDonacion);
-router.delete('/:id', auth.requerido, cancelarDonacion);
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Donaciones:
+ *       type: object
+ *       properties:
+ *         id_donacion:
+ *           type: integer
+ *           description: id autoincrementado para la donacion
+ *         id_usuario:
+ *           type: integer
+ *           description: id del usuario que realiza la donacion
+ *         id_organizacion:
+ *           type: integer
+ *           description: id de la organizacion que recibe la donacion
+ *         cantidad:
+ *           type: integer
+ *           description: cantidad donada
+ *         estatus:
+ *           type: string
+ *           description: estatus de la donacion
+ *       example:
+ *         id_donacion: 1
+ *         id_usuario: 2
+ *         id_organizacion: 3
+ *         cantidad: 300
+ *         estatus: Aceptada
+ */
+
+ /**
+  * @swagger
+  * tags:
+  *   name: Donaciones
+  *   description: Donaciones End Point
+  */
+
+ /**
+ * @swagger
+ * /v1/donaciones:
+ *   post:
+ *     summary: crear nueva donacion
+ *     tags: [Donaciones]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Donaciones'
+ *     responses:
+ *       200:
+ *         description: Donacion creada exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Donaciones'
+ *       401:
+ *         description: error
+ */
+
+router.post('/', crearDonacion);
+
+/**
+ * @swagger
+ * /v1/donaciones/{id}:
+ *  put:
+ *    summary: Modificacion de una donacion
+ *    tags: [Donaciones]
+ *    parameters:
+ *      - in: path
+ *        name: id_donacion
+ *        schema:
+ *          type: integer
+ *        required: true
+ *        description: id de la donacion
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Donaciones'
+ *    responses:
+ *      200:
+ *        description: Modificacion exitosa
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Donaciones'
+ *      401:
+ *        description: Tnot found
+ *      500:
+ *        description: Some error happened
+ */
+
+router.put('/:id', modificarDonacion);
+
+/**
+ * @swagger
+ * /v1/donaciones:
+ *  get:
+ *    summary: creacion de una nueva donacion
+ *    tags: [Donaciones]
+ *    responses:
+ *       200:
+ *         description: Lista de dnonaciones
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Donaciones'
+ *       404:
+ *        description: Lista no encontrada
+ *       500:
+ *         description: Some server error
+ */
+
+router.get('/', consultarDonaciones);
+
+/**
+ * @swagger
+ * /v1/donaciones/{id}:
+ *   get:
+ *     summary: obtener una donacion por id
+ *     tags: [Donaciones]
+ *     parameters:
+ *       - in: path
+ *         name: id_donacion
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: id de la donacion
+ *     responses:
+ *       200:
+ *         description: Donacion por id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Donaciones'
+ *       404:
+ *         description: donacion no encontrada
+ */
+router.get('/:id', consultarDonacion);
+
+/**
+ * @swagger
+ * /v1/donaciones/{id}:
+ *   delete:
+ *     summary: Eliminar una donacion
+ *     tags: [Donaciones]
+ *     parameters:
+ *       - in: path
+ *         name: id_donacion
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: id de la donacion a eliminar
+ * 
+ *     responses:
+ *       200:
+ *         description: Ok
+ *       401:
+ *         description: not found
+ */
+router.delete('/:id', cancelarDonacion);
 
 module.exports = router;
